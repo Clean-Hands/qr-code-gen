6b2d409f352aa1f78cedadb3241027768b7818d0
Lazuli <34384401+Clean-Hands@users.noreply.github.com>
2025-03-06 11:15:48 -0600

    Dynamically chooses the most efficient version and error correction

--- qr-code-gen.py
+++ qr-code-gen.py
@@ -130,11 +130,64 @@ FINDER_PATTERN = [[1,1,1,1,1,1,1],
                   [1,0,0,0,0,0,1],
                   [1,1,1,1,1,1,1]]
 
-ALIGNMENT_PATTERN = [[1,1,1,1,1],
-                     [1,0,0,0,1],
-                     [1,0,1,0,1],
-                     [1,0,0,0,1],
-                     [1,1,1,1,1]]
+
+# capacities = [19,16,13,9,34,28,22,16,55,44,34,26,80,64,48,36,108,86,62,46,136,108,76,60,156,124,88,66,194,154,110,86,232,182,132,100,274,216,154,122,324,254,180,140,370,290,206,158,428,334,244,180,461,365,261,197,523,415,295,223,589,453,325,253,647,507,367,283,721,563,397,313,795,627,445,341,861,669,485,385,932,714,512,406,1006,782,568,442,1094,860,614,464,1174,914,664,514,1276,1000,718,538,1370,1062,754,596,1468,1128,808,628,1531,1193,871,661,1631,1267,911,701,1735,1373,985,745,1843,1455,1033,793,1955,1541,1115,845,2071,1631,1171,901,2191,1725,1231,961,2306,1812,1286,986,2434,1914,1354,1054,2566,1992,1426,1096,2702,2102,1502,1142,2812,2216,1582,1222,2956,2334,1666,1276]
+# capacities = [7, 10, 13, 17, 10, 16, 22, 28]
+# print("[", end="")
+# for i in range(0, len(capacities), 4):
+#     print(f"[{8*capacities[i+3]}, {8*capacities[i+2]}, {8*capacities[i+1]}, {8*capacities[i]}], ", end="")
+# print("]", end="")
+    
+# exit()
+    
+
+# Format: MAX_DATA_BITS[version_number, error_correction_level]
+# where error_correction_level 0=L, 1=M, 2=Q, 3=H
+# but ERR_CORR_LVL 0=M, 1=L, 2=H, 3=Q
+MAX_DATA_BITS_ARR = [[72, 104, 128, 152],
+                     [128, 176, 224, 272],
+                     [208, 272, 352, 440],
+                     [288, 384, 512, 640],
+                     [368, 496, 688, 864],
+                     [480, 608, 864, 1088],
+                     [528, 704, 992, 1248],
+                     [688, 880, 1232, 1552],
+                     [800, 1056, 1456, 1856],
+                     [976, 1232, 1728, 2192],
+                     [1120, 1440, 2032, 2592],
+                     [1264, 1648, 2320, 2960],
+                     [1440, 1952, 2672, 3424],
+                     [1576, 2088, 2920, 3688],
+                     [1784, 2360, 3320, 4184],
+                     [2024, 2600, 3624, 4712],
+                     [2264, 2936, 4056, 5176],
+                     [2504, 3176, 4504, 5768],
+                     [2728, 3560, 5016, 6360],
+                     [3080, 3880, 5352, 6888],
+                     [3248, 4096, 5712, 7456],
+                     [3536, 4544, 6256, 8048],
+                     [3712, 4912, 6880, 8752],
+                     [4112, 5312, 7312, 9392],
+                     [4304, 5744, 8000, 10208],
+                     [4768, 6032, 8496, 10960],
+                     [5024, 6464, 9024, 11744],
+                     [5288, 6968, 9544, 12248],
+                     [5608, 7288, 10136, 13048],
+                     [5960, 7880, 10984, 13880],
+                     [6344, 8264, 11640, 14744],
+                     [6760, 8920, 12328, 15640],
+                     [7208, 9368, 13048, 16568],
+                     [7688, 9848, 13800, 17528],
+                     [7888, 10288, 14496, 18448],
+                     [8432, 10832, 15312, 19472],
+                     [8768, 11408, 15936, 20528],
+                     [9136, 12016, 16816, 21616],
+                     [9776, 12656, 17728, 22496],
+                     [10208, 13328, 18672, 23648]]
+
+# Currently only has data for version 1 and 2
+NUM_OF_ERR_CORR_CODEWORDS_ARR = [[17, 13, 10, 7], 
+                                 [28, 22, 16, 10]]
 
 # data = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 data = "Hello, world!"
@@ -143,13 +196,11 @@ MODE_BITS = "0100" # byte mode
 
 char_count = f'{len(data):08b}' # for byte mode, char_count needs to be 8 bits long
 
-# TODO: figure out which version we should use (https://www.thonky.com/qr-code-tutorial/data-encoding)
 # We are going to use 1L for a proof of concept
-
-VERSION_NUM = 1
-ERR_CORR_LVL = 1 #L
-MAX_DATA_BITS = 19 * 8 # 1L
-NUM_OF_ERR_CORR_CODEWORDS = int(26 - (MAX_DATA_BITS/8)) # this is only for ver 1 QR codes
+# VERSION_NUM = 1
+# ERR_CORR_LVL = 1 #L
+# MAX_DATA_BITS = 19 * 8 # 1L
+# NUM_OF_ERR_CORR_CODEWORDS = int(26 - (MAX_DATA_BITS/8)) # this is only for ver 1 QR codes
 
 
 # convert the characters to ISO 8859-1 encoding
@@ -158,6 +209,35 @@ for char in data:
     combined_enc_str += f'{ord(char):08b}'
 
 data_bits = MODE_BITS + char_count + combined_enc_str
+
+MAX_DATA_BITS = 0
+
+# figure out which version and error correction level we should use
+for i, ver in enumerate(MAX_DATA_BITS_ARR):
+    if ver[0] >= len(data_bits): # H
+        MAX_DATA_BITS = ver[0]
+        NUM_OF_ERR_CORR_CODEWORDS = NUM_OF_ERR_CORR_CODEWORDS_ARR[i][0]
+        VERSION_NUM = i+1
+        ERR_CORR_LVL = 2 # Error correction level H == 2
+        break
+    elif ver[1] >= len(data_bits): # Q
+        MAX_DATA_BITS = ver[1]
+        NUM_OF_ERR_CORR_CODEWORDS = NUM_OF_ERR_CORR_CODEWORDS_ARR[i][1]
+        VERSION_NUM = i+1
+        ERR_CORR_LVL = 3 # Error correction level Q == 3
+        break
+    elif ver[2] >= len(data_bits): # M
+        MAX_DATA_BITS = ver[2]
+        NUM_OF_ERR_CORR_CODEWORDS = NUM_OF_ERR_CORR_CODEWORDS_ARR[i][2]
+        VERSION_NUM = i+1
+        ERR_CORR_LVL = 0 # Error correction level M == 0
+        break
+    elif ver[3] >= len(data_bits): # L
+        MAX_DATA_BITS = ver[3]
+        NUM_OF_ERR_CORR_CODEWORDS = NUM_OF_ERR_CORR_CODEWORDS_ARR[i][3]
+        VERSION_NUM = i+1
+        ERR_CORR_LVL = 1 # Error correction level L == 1
+        break
 
 # add up to 4 zeroes as a terminator, making sure we don't go over the max length
 i = 0
